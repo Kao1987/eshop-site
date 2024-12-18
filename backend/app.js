eb1027033009787d16e1f7a697002b10a1ecc022
Kao1987 <winterlove1999@gmail.com>
2024-12-18 14:46:22 +0800

    更新

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 // backend/app.js
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose'); 
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
