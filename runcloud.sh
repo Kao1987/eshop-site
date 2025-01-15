0e1bca0eba7c6deccb7a5b6f8248077987d96955
Kao1987 <winterlove1999@gmail.com>
2025-01-15 09:48:43 +0800

    Remove .sh files from tracking

--- runcloud.sh
+++
@@ -1,3 +0,0 @@
-docker buildx build --platform linux/amd64 -t kao1987/ecshop:1.1.4 --push .
-
-
--- test.sh
+++
@@ -1,5 +0,0 @@
-cd frontend
-yarn build
-cd ..
-rsync -avz --delete -e "ssh -i ~/.ssh/user" backend/ user@34.169.83.101:/home/winterlove1999/newshop
-
